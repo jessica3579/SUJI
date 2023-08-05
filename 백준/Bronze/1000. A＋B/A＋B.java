6052045ca3760f40545f8cdb6d19246aa09439e8
SUJI LEE <85113767+jessica3579@users.noreply.github.com>
2023-08-05 20:46:05 +0900

    [Bronze V] Title: A+B, Time: 128 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,9 +1,14 @@
-import java.util.*;
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        System.out.print(a + b);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        bw.write(String.valueOf(a+b));
+        bw.close();
     }
 }
